7c12d88a5dafa5e047fa1f6d2b8faf68036a941b
Nyckolas12 <nmendez@student.neumont.edu>
2024-07-05 18:50:00 -0600

    fireworks

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -4,6 +4,9 @@
 #include "../../Engine/Source/Vector2.h"
 #include <vector>
 #include "Input.h"
+#include "Particle.h"
+#include "Random.h"
+#include "ETime.h"
 using namespace std;
 
 
@@ -15,10 +18,17 @@ int main(int argc, char* argv[])
 	renderer.CreateWindow("Game Engine", 800, 600);
 	Input input;
 	input.Initialize();
+	Time time;
 
 
-	Vector2 v1{ 400,300 };
-	Vector2 v2{ 700,500 };
+	vector<Particle> particles;
+	for (int i = 0; i < 0; i++)
+	{
+		//particles.push_back(Particle{ {rand() % 800,rand() % 800}, {randomf(100,300),0.0f}});
+	}
+
+	//Vector2 v1{ 400,300 };
+	//Vector2 v2{ 700,500 };
 
 	vector<Vector2> points;
 	/*for (int i = 0; i < 100; i++)
@@ -30,6 +40,8 @@ int main(int argc, char* argv[])
 		bool quit = false;
 	while (!quit)
 	{
+		time.Tick();
+		cout << time.GetTime() << endl;
 		//Input
 		input.Update();
 
@@ -40,8 +52,26 @@ int main(int argc, char* argv[])
 
 		//UPDATE
 		Vector2 mousePosition = input.GetMousePosition();
+		if (input.GetMouseButtonDown(0))
+		{
+	
+			//particles.push_back(Particle{ mousePosition,{randomf(-300,300),randomf(-300,300)} });
+			particles.push_back(Particle{ mousePosition,{randomf(-300,300),randomf(-300,300)},randomf(1,5) });
+
+			
+		}
+
+		
+		for (Particle& particle : particles)
+		{
+			particle.Update(time.GetDeltaTime());
+			if (particle.position.x > 800) particle.position.x = 0;
+			if (particle.position.x < 0) particle.position.x = 800;
+
+			if (particle.lifespan != 0) particle.lifespan -= time.GetDeltaTime();
+		}
 		//cout << mousePosition.x << " " << mousePosition.y << endl;
-		if (input.GetMouseButtonDown(0)&& !input.GetPreviousMouseButtonDown(0))
+		/*if (input.GetMouseButtonDown(0)&& !input.GetPreviousMouseButtonDown(0))
 		{
 			cout << "mouse down\n";
 			points.push_back(mousePosition);
@@ -55,7 +85,7 @@ int main(int argc, char* argv[])
 				points.push_back(mousePosition);
 			}
 			points.push_back(mousePosition);
-		}
+		}*/
 		/*Vector2 speed{ 0.2f, -0.2f };
 		for (Vector2& point : points)
 		{
@@ -70,17 +100,17 @@ int main(int argc, char* argv[])
 
 		// draw line
 		renderer.SetColor(rand() % 256, rand() % 256, rand() % 256, rand() % 256);
-	/*	renderer.DrawLine(300, 200, 400, 200);
-		renderer.DrawLine(400, 200, 400, 200);
-		renderer.DrawLine(500, 200, 400, 200);
-		renderer.DrawLine(600, 200, 400, 200);*/
+		for (Particle particle : particles)
+		{
+			if(particle.lifespan >= 0) particle.Draw(renderer);
+		}
 
 		for (int i = 0; points.size()>1 && i < points.size()-1; i++)
 		{
 			renderer.DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
 		}
 
-		renderer.DrawLine(v1.x, v1.y, v2.x, v2.y);
+		//renderer.DrawLine(v1.x, v1.y, v2.x, v2.y);
 
 		/*for (int i = 0; i < 1000; i++)
 		{
